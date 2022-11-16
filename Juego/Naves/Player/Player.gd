9aff585d1f549b4d3f273d2bbe6387d138e17f56
prietodactyl <agus.prieto.93@gmail.com>
2022-11-15 21:47:58 -0300

    Animacion spawn y estados jugador

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -1,6 +1,9 @@
 class_name Player
 extends RigidBody2D
 
+## Enums
+enum ESTADO{SPAWN, VIVO, INVENCIBLE, MUERTO}
+
 ## Atributos export
 export var potencia_motor:int = 20
 export var potencia_rotacion:int = 280
@@ -9,14 +12,24 @@ export var estela_maxima:int = 150
 ## Atributos
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
+var estado_actual:int = ESTADO.SPAWN
 
 ## Atributos onready
 onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
 onready var estela:Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx:Motor = $MotorSFX
+onready var colisionador:CollisionShape2D = $CollisionShape2D
 
+## Señales internas
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "spawn":
+		controlador_estados(ESTADO.VIVO)
+	
 ## Métodos
+func _ready() -> void:
+	pass
+
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
 	apply_central_impulse(empuje.rotated(rotation))
@@ -25,6 +38,9 @@ func _process(delta: float) -> void:
 	player_input()
 
 func _unhandled_input(event: InputEvent) -> void:
+	if not esta_input_activo():
+		return
+			
 	# Disparo Rayo
 	if event.is_action("disparo_secundario"):
 		laser.set_is_casting(true)
@@ -44,7 +60,34 @@ func _unhandled_input(event: InputEvent) -> void:
 		motor_sfx.sonido_off()
 	
 ## Métodos Custom
+func controlador_estados(nuevo_estado:int) -> void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			colisionador.set_deferred("disabled", false)
+			canion.set_puede_disparar(true)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled", true)
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(false)
+			queue_free()
+		_:
+			printerr("Error de estado")
+	
+	estado_actual = nuevo_estado
+
+func esta_input_activo() -> bool:
+	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
+		return false
+	return true
+
 func player_input() -> void:
+	if not esta_input_activo():
+		return
+		
 	# Empuje
 	empuje = Vector2.ZERO
 	if Input.is_action_pressed("Mover Adelante"):
@@ -65,3 +108,4 @@ func player_input() -> void:
 	
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
+
