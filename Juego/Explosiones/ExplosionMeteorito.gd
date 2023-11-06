7110039295fa00c6585b39ef203d41d2dc72ca18
prietodactyl <agus.prieto.93@gmail.com>
2023-11-05 23:36:10 -0300

    Pulido de tercer semana

--- Juego/Explosiones/ExplosionMeteorito.gd
+++ Juego/Explosiones/ExplosionMeteorito.gd
@@ -2,19 +2,21 @@ class_name ExplosionMeteorito
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+onready var animations := $AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	animations.play(elegir_explosion_aleatoria())
+	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func elegir_explosion_aleatoria() -> String:
+	randomize()
+	var num_anim:int = animations.get_animation_list().size() - 1
+	var indice_anim_aleatoria:int = randi() % num_anim + 1
+	var lista_anim:Array = animations.get_animation_list()
+	
+	return lista_anim[indice_anim_aleatoria]
 
 func _on_AnimationPlayer_animation_finished(_anim_name: String) -> void:
 	queue_free()
